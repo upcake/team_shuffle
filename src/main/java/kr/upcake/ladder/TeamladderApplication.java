515781e0d04d6aad93c48ab6f9bbd3c4c4940534
upcake <bermwoo@gmail.com>
2021-06-29 17:16:31 +0900

    first commit

---
+++ src/main/java/kr/upcake/ladder/TeamladderApplication.java
@@ -0,0 +1,13 @@
+package kr.upcake.ladder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamladderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamladderApplication.class, args);
+	}
+
+}
---
+++ src/main/java/kr/upcake/ladder/controller/HomeController.java
@@ -0,0 +1,18 @@
+package kr.upcake.ladder.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping(value="/")
+	public String home(Model model) {
+		String[] teamList = {"AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG", "HHH", "III", "JJJ", "KKK", "LLL", "MMM", "NNN", "OOO", "PPP"};
+		
+		model.addAttribute("teamList", teamList);
+		
+		return "index";
+	}
+}
